b75e82bfbdeb8c788866057c1db60645f8f90a75
KenBan09 <keeno.pkt@gmail.com>
2024-12-17 15:55:26 +0700

    lab10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+    string textline;
+    int count = 0;
+    float sum = 0;
+    float mean,num2;
+    ifstream source;
+    source.open ("score.txt");
+
+    while (getline(source,textline)){
+
+        sum += atof(textline.c_str());
+
+        num2 += pow(atof(textline.c_str()),2) ;
+
+        count++;
+
+    }
+    mean = sum/count; 
+
+    cout << "Number of data = " << count << "\n";
+
+    cout << setprecision(3);
+
+    cout << "Mean = " << mean << "\n";
+
+    cout << "Standard deviation = " << sqrt((num2/count)-pow(mean,2));
+
+}
